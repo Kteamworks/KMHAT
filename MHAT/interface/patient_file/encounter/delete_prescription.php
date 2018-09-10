18a59db474201234f45c46d5af04599bb414eee2
PavithraSrinivas <PavithraSrinivas@users.noreply.github.com>
2018-09-10 13:08:07 +0530

    Changes on presciptions

---
+++ MHAT/interface/patient_file/encounter/delete_prescription.php
@@ -0,0 +1,113 @@
+@@ -0,0 +1,112 @@
+
+
+<?php
+
+include_once("../../globals.php");
+
+
+$id = (isset($_REQUEST['id'])) ? $_REQUEST['id'] : '';
+// when the Cancel button is pressed, where do we go?
+$returnurl = $GLOBALS['concurrent_layout'] ? 'encounter_top.php' : 'patient_encounter.php';
+
+if ($_POST['confirm']) {
+    // set the delete flag of the indicated form
+	 sqlStatement("update prescriptions set deleted=1,deleted_user='".$_SESSION['authUser']."' where id='".$id."'");
+    // log the event   
+    newEvent("delete", $_SESSION['authUser'], $_SESSION['authProvider'], 1, "Form ".$_POST['formname']." deleted from Encounter ".$id);
+
+    // redirect back to the encounter
+    $address = "{$GLOBALS['rootdir']}/patient_file/encounter/$returnurl";
+    echo "\n<script language='Javascript'>top.restoreSession();window.location='$address';</script>\n";
+    exit;
+}
+?>
+<html>
+
+<head>
+<?php html_header_show();?>
+<link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
+<link rel="stylesheet" href="<?php echo $GLOBALS['webroot'] ?>/library/js/jAlert-master/src/jAlert-v3.css" />
+<link rel="stylesheet" href="<?php echo $GLOBALS['webroot'] ?>/library/js/jquery.treeview-1.4.1/jquery.treeview.css" />
+<script src="<?php echo $GLOBALS['webroot'] ?>/library/js/jquery-1.7.2.min.js"></script>
+<script src="<?php echo $GLOBALS['webroot'] ?>/library/js/jAlert-master/src/jAlert-v3.js"></script>
+<script src="<?php echo $GLOBALS['webroot'] ?>/library/js/jAlert-master/src/jAlert-functions.js"> //optional!!</script>
+<!-- supporting javascript code -->
+<!--<script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/js/jquery.js"></script>-->
+
+<script type="text/javascript" src="../../../library/dialog.js"></script>
+<!-- pop up calendar -->
+<style type="text/css">@import url(<?php echo $GLOBALS['webroot'] ?>/library/dynarch_calendar.css);</style>
+<script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/dynarch_calendar.js"></script>
+<?php include_once("{$GLOBALS['srcdir']}/dynarch_calendar_en.inc.php"); ?>
+<script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/dynarch_calendar_setup.js"></script>
+<script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/textformat.js"></script>
+<script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/dialog.js"></script>
+<link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
+
+</head>
+
+<body class="body_top">
+
+<span class="title">Delete Prescription</span>
+
+<form method="post" action="<?php echo $rootdir;?>/patient_file/encounter/delete_prescription.php" name="my_form" id="my_form">
+<?php
+// output each GET variable as a hidden form input
+foreach ($_GET as $key => $value) {
+    echo '<input type="hidden" id="'.$key.'" name="'.$key.'" value="'.$value.'"/>'."\n";
+}
+?>
+<input type="hidden" id="confirm" name="confirm" value="1"/>
+<p>
+<?php
+$pres_id=sqlStatement("SELECT * from prescriptions where id='".$id."'");
+$pres_id1=sqlFetchArray($pres_id);
+$pres_id2=$pres_id1['drug'];
+?>
+You are about to delete a Prescription: '<?php echo attr($pres_id2);?>'
+</p>
+<table>
+<?php
+$today = date('Y-m-d H:i:s',strtotime("+0 days"));
+?>
+</table>
+<input type="button" id="confirmbtn" name="confirmbtn" value="Yes, Delete this Prescription">
+<input type="button" id="cancel" name="cancel" value="Cancel">
+</form>
+
+</body>
+<script language="javascript">
+/* required for popup calendar */
+//Calendar.setup({inputField:"admit_date", ifFormat:"%Y-%m-%d", button:"img_transfer_date"});
+//Calendar.setup({inputField:"discharge_date", ifFormat:"%Y-%m-%d %H:%M:%S", button:"img_end_date",showsTime:'true'});
+</script>
+
+<script language="javascript">
+// jQuery stuff to make the page a little easier to use
+
+$(document).ready(function(){
+    $("#confirmbtn").click(function() { return ConfirmDelete(); });
+    $("#cancel").click(function() { location.href='<?php echo "$rootdir/patient_file/encounter/$returnurl";?>'; });
+});
+
+function ConfirmDelete() {
+	 $.jAlert({'type': 'confirm','confirmQuestion':'Are you sure you wish to Delete this Prescription? ', 'onConfirm': function(){
+        top.restoreSession();
+        $("#my_form").submit();
+		 parent.location.reload();
+        return true;   
+  }, 'onDeny': function(){
+    return false;    
+  } });
+    /*if (confirm("This action cannot be undone. Are you sure you wish to discharge this Patient?")) {
+        top.restoreSession();
+        $("#my_form").submit();
+        return true;
+    }
+    return false;*/
+}
+
+</script>
+
+</html>
